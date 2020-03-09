5e2b6829bef245fe504ca89dc229c024986a2917
maxiao175 <maxiao8@360buy.com>
2020-03-09 12:03:00 +0800

    publish

---
+++ build/precache-manifest.3162154fab68a82cc4363d929a37969c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad69f244efaa40637bb7f9baed0c8bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2484fd7ff9cd8e719561",
+    "url": "/static/css/2.563af9af.chunk.css"
+  },
+  {
+    "revision": "accabe82a19918186c4e",
+    "url": "/static/css/main.c52f2f86.chunk.css"
+  },
+  {
+    "revision": "2484fd7ff9cd8e719561",
+    "url": "/static/js/2.f9009bb4.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.f9009bb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "accabe82a19918186c4e",
+    "url": "/static/js/main.e5977e95.chunk.js"
+  },
+  {
+    "revision": "84587511ee0fcbdf09c3",
+    "url": "/static/js/runtime-main.aa35abcb.js"
+  }
+]);
